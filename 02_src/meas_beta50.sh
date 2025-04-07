4921cf4097721cd4e95857b437cdccb582299653
alelevente <ale.levente@gmail.com>
2025-04-07 12:57:46 +0200

    long measurements

---
+++ 02_src/meas_beta50.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+seeds=(42 1234 1867 613 1001)
+
+for s in ${seeds[@]}; do
+    python3 auction_measurement.py $s --name beta50_$s --config ../01_simulation/configurations/beta50.json &
+done
